e6678bcc6073cbdf7fef2994ecc7ea3f42deda0a
LAPTOP_JIE\jiema <jiemartinez12@gmail.com>
2022-12-01 08:31:38 +0100

    primer commit

---
+++ app/src/main/java/com/example/mazeapp/GUI/CrearComanda.java
@@ -0,0 +1,18 @@
+package com.example.mazeapp.GUI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mazeapp.R;
+
+public class CrearComanda extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_comanda);
+    }
+}
