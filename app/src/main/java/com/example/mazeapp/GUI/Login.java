24cf7991900733c80987a034961db7aebcd1f570
LAPTOP-VTECL76I\olatz <dam32.olatz.cano@gmail.com>
2022-12-07 12:25:52 +0100

    Terminado el diseño del login. Falta funcionalidad.

--- app/src/main/java/com/example/mazeapp/GUI/Login.java
+++ app/src/main/java/com/example/mazeapp/GUI/Login.java
@@ -2,25 +2,30 @@ package com.example.mazeapp.GUI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import com.example.mazeapp.R;
 
-public class MainActivity extends AppCompatActivity {
-
-    String user;
-
+public class Login extends AppCompatActivity {
+    Button btAcceder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
-        //if(String user == null)//abre login;{
+        btAcceder = findViewById(R.id.btAcceder);
+    }
 
+    public void login(View v){
+        Intent opc = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(opc);
     }
 }
