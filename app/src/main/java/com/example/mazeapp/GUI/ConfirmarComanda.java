2ace03bacb498353a03380700e6af5f2c4406e44
LAPTOP-VTECL76I\olatz <dam32.olatz.cano@gmail.com>
2022-12-18 11:03:13 +0100

    boton "atras" en confirmar comanda

--- app/src/main/java/com/example/mazeapp/GUI/ConfirmarComanda.java
+++ app/src/main/java/com/example/mazeapp/GUI/ConfirmarComanda.java
@@ -3,14 +3,18 @@ package com.example.mazeapp.GUI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.mazeapp.R;
 
 public class ConfirmarComanda extends AppCompatActivity {
-
+    ImageButton btsalir2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmar_comanda);
+        btsalir2 = findViewById(R.id.btsalir2);
     }
+    public void salir2(View v){finish();}
 }
